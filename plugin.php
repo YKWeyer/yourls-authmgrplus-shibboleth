ceebfb78643d0e4c0dcfacf823f8a43e5c2f97a4
Yann Weyer <yann.weyer@tu-berlin.de>
2020-07-14 18:48:17 +0200

    Split login logic between `is_valid_user` and `pre_login` actions

--- plugin.php
+++ plugin.php
@@ -33,37 +33,24 @@ function shibboleth_check_authMgrPlus()
     }
 }
 
-// Hook our custom function into the 'shunt_is_valid_user' filter
-yourls_add_filter( 'shunt_is_valid_user', 'shibboleth_is_valid_user' );
-
-function shibboleth_is_valid_user() {
-        global $yourls_user_passwords;
-        // Check for attributes set by mod_shib
-        if (isset( $_SERVER[SHIBBOLETH_UID] ) && isset( $_SERVER[SHIBBOLETH_ENTITLEMENT] ) && 
-                // Check if entitlement matches regex
-                preg_match(SHIBBOLETH_ENTITLEMENT_REGEX, $_SERVER[SHIBBOLETH_ENTITLEMENT]))
-        {
-                //
-                shibboleth_initAssignment();
-
-                // Notify various yourls stages
-                yourls_do_action( 'pre_login' );
-                yourls_do_action( 'pre_login_username_password' );
-                yourls_do_action( 'login' );
-                yourls_set_user( $_SERVER[SHIBBOLETH_UID] );
-                if ( !yourls_is_API() ) {
-                        // Satisfy yourls' cookie generation routine
-                        $yourls_user_passwords[$_SERVER[SHIBBOLETH_UID]]=md5($_SERVER[SHIBBOLETH_UID]);
-                        yourls_store_cookie( YOURLS_USER );
-                }
-                return true;
-        }
-        return null;
+// Hook our custom function into the 'is_valid_user' filter
+yourls_add_filter( 'is_valid_user', 'shibboleth_is_valid_user' );
+function shibboleth_is_valid_user($unfiltered_valid) {
+    // Check for attributes set by mod_shib
+    if (isset( $_SERVER[SHIBBOLETH_UID] ) && isset( $_SERVER[SHIBBOLETH_ENTITLEMENT] ) &&
+        // Check if entitlement matches regex
+        preg_match(SHIBBOLETH_ENTITLEMENT_REGEX, $_SERVER[SHIBBOLETH_ENTITLEMENT]))
+    {
+        yourls_set_user( $_SERVER[SHIBBOLETH_UID] );
+        return true;
+    }
+    return $unfiltered_valid;
 }
 
 /**
  * Populate $amp_role_assignment array with corresponding roles
  */
+yourls_add_action('pre_login', 'shibboleth_initAssignment');
 function shibboleth_initAssignment(){
     global $amp_role_assignment;
     global $shibboleth_rbac_role_assignment;
